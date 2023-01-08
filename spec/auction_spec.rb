0f2aba2ab0e6aea611db4e9810a7ea263b3466bc
Melony Franchini <meltravelz@gmail.com>
2023-01-08 14:57:06 -0700

    Add initialize tests for auction

--- spec/auction_spec.rb
+++ spec/auction_spec.rb
@@ -3,6 +3,23 @@ require './lib/item'
 require './lib/attendee'
 require './lib/auction'
 
+RSpec.describe Auction do
+  let(:auction) { Auction.new }
 
-let(:item2) { Item.new('Bamboo Picture Frame') }
-# expect().to eq()
+  let(:item1) { Item.new('Chalkware Piggy Bank') }
+  let(:item2) { Item.new('Bamboo Picture Frame') }
+
+  let(:attendee) { Attendee.new(name: 'Megan', budget: '$50') }
+
+  describe "#initialize" do
+    it "exists" do
+      expect(auction).to be_instance_of(Auction)
+    end
+
+    it "has attributes" do
+      expect(auction.items).to eq([])
+    end
+  end
+
+  # expect().to eq()
+end
